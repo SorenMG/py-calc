efaf72830b6953860da3e90753e33930082e4571
SorenMG <soren1146@gmail.com>
2019-10-02 23:13:55 +0200

    Add help command Add color to result Add solve equation command

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from cmd import Cmd
+import cmd, sys
+from sympy.solvers import solve
+from colors import *
+import os
 
-class Calc(Cmd):
-    promt = '>> '
+# Integrate
+# Derivative
+# Solve
 
-    def do_help(self, arg):
-        print('Help incomming')
-        return True
-    
+class CalcShell(cmd.Cmd):
+    intro = 'If confused, type "help" before a command'
+    prompt = 'Calc >> '
+    file = None
+
+    def printEquation(self, eq):
+        sys.stdout.write(GREEN)
+        print('\n')
+        for e in eq:
+            print(e)
+            print('\n')
+        sys.stdout.write(RESET)
+
+    # Prints out commands
+    def do_help(self, args):
+        print(' - Solve:', self.do_solve.__doc__)
+
+    # Solve equation for x
+    def do_solve(self, arg):
+        """Solves the given equation"""
+        self.printEquation(solve(arg))
+
 if __name__ == '__main__':
-    Calc().cmdloop()
+    CalcShell().cmdloop()
