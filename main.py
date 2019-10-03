4b99e220f98cccd7bde99931ff740f541b6a2800
SorenMG <soren1146@gmail.com>
2019-10-03 14:04:20 +0200

    Add solve method documentation Add integrate method

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cmd, sys
-from sympy.solvers import solve
+from sympy import *
 from sympy import Symbol
 from colors import *
-import os
+import re
 
 # Integrate
 # Derivative
@@ -16,29 +16,48 @@ class CalcShell(cmd.Cmd):
     def printEquation(self, eq):
         sys.stdout.write(GREEN)
         print('\n')
-        for e in eq:
-            print(e)
-            print('\n')
+        print(eq)
+        print('\n')
         sys.stdout.write(RESET)
 
+    def processInput(self, eq):
+        eq = re.sub(r"\s+", "", eq)
+        return eq.split(',')
+        
     # Prints out commands
     def do_help(self, args):
         print(' - Solve:', self.do_solve.__doc__)
 
     # Solve equation for x
     def do_solve(self, arg):
-        """Solves the given equation"""
+        """Solves the given equation.\n
+        Definition:
+        solve equation, variable
+        """
         # Get variable to solve for
-        arg.replace(" ", "")
-        argData = arg.split(',')
+        argData = self.processInput(arg)
         solvedEq = ""
         if len(argData) == 2:
-            print(argData)
             solvedEq = solve(argData[0], Symbol(argData[1]))
         else:
             solvedEq = solve(argData[0])
          
         self.printEquation(solvedEq)
+
+    def do_integrate(self, arg):
+        """Integrates the given equation.\n
+        Definition:
+        integrate equation, variable
+        """
+        argData = self.processInput(arg)
+        solvedEq = ""
+        if len(argData) == 2:
+            solvedEq = integrate(argData[0], Symbol(argData[1]))
+        else:
+            solvedEq = solve(argData[0])
+        
+        self.printEquation(solvedEq)
+            
 
 if __name__ == '__main__':
     CalcShell().cmdloop()
