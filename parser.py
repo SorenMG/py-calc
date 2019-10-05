16a3f073afd7a4c3257117e4eb819940b54ccd29
SorenMG <soren1146@gmail.com>
2019-10-05 18:18:27 +0200

    Removed manual parsing and are now using inbeded parser in Sympy

--- parser.py
+++ parser.py
@@ -1,12 +1,8 @@
 # Equation Parser Module
-from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, split_symbols, convert_xor, factorial_notation
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, split_symbols, convert_xor, factorial_notation, convert_equals_signs
+from sympy import Eq
+
+transformations = (standard_transformations + (implicit_multiplication_application,) + (split_symbols,) + (convert_xor,) + (factorial_notation,) + (convert_equals_signs,))
 
 def parse(inputEquation):
-    transformations = (standard_transformations + (implicit_multiplication_application,) + (split_symbols,) + (convert_xor,) + (factorial_notation,))
-    
-    # Insert all from the right of the = sign on the left
-    if (inputEquation.find('=') != -1):
-        splittedEq = inputEquation.split('=')
-        inputEquation = splittedEq[0] + '-(' + splittedEq[1] + ')'
-
     return parse_expr(inputEquation, transformations=transformations)
