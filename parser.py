05fa5540fe09bac57cce75c70cd574c0bb92714d
SorenMG <soren1146@gmail.com>
2019-10-07 01:35:12 +0200

    Simplified parser so it returns None on the second index if no variable is present

--- parser.py
+++ parser.py
@@ -1,12 +1,18 @@
 # Equation Parser Module
-from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, split_symbols, convert_xor, factorial_notation
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, split_symbols, convert_xor, factorial_notation, convert_equals_signs
 
-def parse(inputEquation):
-    transformations = (standard_transformations + (implicit_multiplication_application,) + (split_symbols,) + (convert_xor,) + (factorial_notation,))
-    
-    # Insert all from the right of the = sign on the left
-    if (inputEquation.find('=') != -1):
-        splittedEq = inputEquation.split('=')
-        inputEquation = splittedEq[0] + '-(' + splittedEq[1] + ')'
+transformations = (standard_transformations + (implicit_multiplication_application,) + (split_symbols,) + (convert_xor,) + (factorial_notation,) + (convert_equals_signs,))
 
-    return parse_expr(inputEquation, transformations=transformations)
+def parse(equation):
+    equation = equation.replace(" ", "")
+    # Split to isolate variable
+    equation = equation.split(',')
+    # Parse the equation
+    for i in range(0, len(equation)):
+        equation[i] = parse_expr(equation[i], transformations=transformations)
+
+    # If no variable parsed, return none
+    if len(equation) != 2:
+        equation.append(None)
+
+    return equation
