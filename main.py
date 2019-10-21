1166e89bf449594097ac9b468eaec50de424bd71
SorenMG <soren1146@gmail.com>
2019-10-21 15:56:47 +0200

    Main file now handles parsing

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import cmd
+import sys
 from src.calc import integrate, evaluation, derivative, solve
+from src.parser import parser
 
-class CalcShell(cmd.Cmd):
+class WolfShell(cmd.Cmd):
     prompt = '>> '
 
-    def emptyline(self):
-        pass 
-    def onecmd(self, line):
+    def emptyinput(self):
+        pass
+    def onecmd(self, input):
         """Calculate the equation 'wolfram-like'"""
+        input = parser.parseEquation(input)
         # Try everything
-        solve.solve(line)
-        integrate.integrate(line)
-        derivative.diff(line)
-        evaluation.eval(line)
-            
+        if len(input) <= 1:
+            input = input[0]
+            solve.solve(input)
+            integrate.integrate(input)
+            derivative.diff(input)
+            evaluation.eval(input)
+        else:
+            pass
+
 if __name__ == '__main__':
-    CalcShell().cmdloop()
+    WolfShell().cmdloop()
