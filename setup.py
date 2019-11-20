adcd7e4235425da6ac75de5f6a72801a9ea368be
SorenMG <soren1146@gmail.com>
2019-11-20 08:56:37 +0100

    Housework

---
+++ setup.py
@@ -0,0 +1,5 @@
+from calc.integrate import indefiniteIntegral, definiteIntegral
+from calc.derivative import derivative
+from calc.evaluation import evaluate
+from calc.limit import limit
+from calc.solve import solve
---
+++ shell.py
@@ -0,0 +1,33 @@
+import cmd
+
+
+class WolfShell(cmd.Cmd):
+    prompt = '>> '
+
+    def emptyinput(self):
+        pass
+
+    # def onecmd(self, input):
+        # """Calculate the equation 'wolfram-like'"""
+        # input = parser.parseEquation(input)
+
+        # printer.printAnswer('Input', input)
+
+        # # Try everything
+        # if len(input) <= 1:
+            # input = input[0]
+            # solve.solve(input)
+            # integrate.integrate(input)
+            # derivative.diff(input)
+            # evaluation.eval(input)
+        # # Calulate with interval
+        # else:
+            # parsed, start, end = (e for e in input)
+            # start = int(start)
+            # end = int(end)
+            # print(parsed, start, end)
+            # solve.solve(parsed)
+            # integrate.integrate(parsed, start, end)
+
+# if __name__ == '__main__':
+    # WolfShell().cmdloop()
